acc3a8a37c5f5a0ea61b8aa67fced2137de3656a
Pablo Zivic <pablo.rzivic@mercadolibre.com>
2017-05-19 23:23:52 -0300

    Much better portfolio selection

--- fondos/portfolio_selection.py
+++ fondos/portfolio_selection.py
@@ -14,9 +14,9 @@ def normalize(weights):
     return [w / total for w in weights]
 
 
-def portfolio_space(start_date, end_date, tickers, max_portfolio_size):
+def portfolio_space(start_date, end_date, tickers, min_portfolio_size, max_portfolio_size):
     portfolio_options = []
-    for portfolio_size in xrange(1, max_portfolio_size + 1):
+    for portfolio_size in xrange(min_portfolio_size, max_portfolio_size + 1):
         indices = []
         weights = []
         for ticker_index in xrange(portfolio_size):
@@ -60,24 +60,46 @@ def loss_function(params):
     quotes = []
     weights = []
     for i, ticker_index in enumerate(params['portfolio']['indices']):
-        try:
-            quote = Quote(tickers.pop(int(ticker_index)))
-        except: import ipdb;ipdb.set_trace()
+        quote = Quote(tickers.pop(int(ticker_index)))
         quotes.append(quote)
         weights.append(params['portfolio']['weights'][i])
 
     portfolio = Portfolio(quotes, weights)
 
     start_date = params['start_date']
-    portfolio_return = portfolio.get_returns(start_date=start_date)[params['end_date']:].dropna().iloc[0]
+    end_date=params['end_date']
+
+    portfolio_return = portfolio.get_returns(
+        start_date=start_date,
+        end_date=end_date
+    ).dropna().iloc[0]
+
     portfolio_volatility = portfolio.get_volatility(start_date=start_date)
-    loss = - (portfolio_return - 0.05 * portfolio_volatility)
-    if np.isnan(loss): import ipdb;ipdb.set_trace()
-    print "Loss: {:<3}".format(loss)
-    return dict(
+    too_imbalanced = max(0, (0.5 - min(weights) * len(weights)))
+
+    returns = [
+        quote.get_returns(start_date=start_date, end_date=end_date)
+        for quote in portfolio.quotes
+    ]
+    pairwise_correlations = []
+    for i, r1 in enumerate(returns):
+        for r2 in returns[i+1:]:
+            pairwise_correlations.append(r1.corr(r2))
+
+    loss = (
+        - portfolio_return
+        + 0.05 * portfolio_volatility
+        + too_imbalanced
+        + 0.05 * np.max(pairwise_correlations)
+    )
+
+    res = dict(
         loss=loss,
+        portfolio_return=portfolio_return,
+        portfolio_volatility=portfolio_volatility,
         status=STATUS_OK,
         portfolio=portfolio.to_dict(),
     )
 
-
+    pprint(res)
+    return res
